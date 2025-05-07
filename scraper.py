766d2d2b3d1f8bb0cf34e27acd9dbf96fb9d3f5e
HokulaniTopping <hokulanigit@gmail.com>
2025-05-07 15:33:44 -0700

    debugging in extract links

--- scraper.py
+++ scraper.py
@@ -104,6 +104,7 @@ def extract_next_links(url, resp):
 
     try:
         if resp.status != 200 or resp.raw_response is None:
+            logger.warning(f"❗Skipping {url} due to status {resp.status} or missing response")
             return []
 
         soup = BeautifulSoup(resp.raw_response.content, "lxml")
